7d59b778d341a2dcedd9cf73d860749f732194ab
juliansand <sandoval.julian10@gmail.com>
2018-03-09 19:06:05 -0600

    first commit

---
+++ src/farmacia_cliente/Farmacia_cliente.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package farmacia_cliente;
+
+/**
+ *
+ * @author julian
+ */
+public class Farmacia_cliente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal prin = new Principal();
+        prin.show(true);
+    }
+    
+}
